08f5e08a9554150a00654439840c05e2d5f0e20a
Samir Gonzalo Aes Lopez <17470067@campeche.tecnm.mx>
2021-10-29 20:43:31 -0500

    Implementa la sincronizacíon usando Data Biding

--- ContadorManual/ContadorManual/MainPage.xaml.cs
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -5,28 +5,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ContadorManual.Model;
 
 namespace ContadorManual
 {
     public partial class MainPage : ContentPage
     {
         private int _Conteo;
+        private Contador _Contador;
         public MainPage()
         {
             InitializeComponent();
-            _Conteo = 0;
+            //_Conteo = 0;
+            _Contador = new Contador();
+            //ConteoLabel.Text = _Contador.Conteo.ToString();
+            ConteoLabel.BindingContext = _Contador;
+
         }
 
         private void ReiniciarButton_Clicked(object sender, EventArgs e)
         {
-            _Conteo++;
-            ConteoLabel.Text = _Conteo.ToString();
+            //_Conteo++;
+            //ConteoLabel.Text = _Conteo.ToString();
+
+            _Contador.ReiniciarConteo();
+            //ConteoLabel.Text = _Contador.Conteo.ToString();
+
         }
 
         private void ContarButton_Clicked(object sender, EventArgs e)
         {
-            _Conteo = 0;
-            ConteoLabel.Text = _Conteo.ToString();
+            //_Conteo = 0;
+            //ConteoLabel.Text = _Conteo.ToString();
+
+            _Contador = new Contador();
+            //ConteoLabel.Text = _Contador.Conteo.ToString();
+
         }
     }
 
--- ContadorManual/ContadorManual/Model/Contador.cs
+++ ContadorManual/ContadorManual/Model/Contador.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace ContadorManual.Model
 {
-    class Contador
+    class Contador : INotifyPropertyChanged
     {
+        private int _Conteo;
+
+        public event PropertyChangingEventHandler PropertyChanged;
+        public int Conteo
+        {
+            get => _Conteo;
+            set
+            {
+                if (_Conteo != value)
+                {
+                    _Conteo = value;
+                    OnPropertyChanged("Conteo");
+                }
+            }
+        }
+        public Contador()
+        {
+            ReiniciarConteo();
+        }
+        public void Contar()
+        {
+            Conteo++;
+        }
+        public void ReiniciarConteo()
+        {
+            Conteo = 0;
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
+
 }
